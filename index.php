b875b65c9c02d95ce182cb827e4025ff4b125885
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-26 13:50:29 +0200

    [add]: $hotels foreach cycle, print infos in card

--- index.php
+++ index.php
@@ -55,20 +55,37 @@
 <body>
   
   <div class="container my-5">
-    <div class="row">
+    <div class="row justify-content-center">
 
-      <?php //foreach ($hotels as $hotel): ?>
-      <div class="col">
+      <?php foreach ($hotels as $hotel): ?>
+      <div class="col-6">
 
-        <div class="card" style="width: 18rem;">
+        <div class="card my-3 w-100" style="width: 18rem;">
 
           <div class="card-body">
 
-            <h5 class="card-title border-bottom pb-2"> Nome albergo <?php //echo $hotel['name'] ?> </h5>
-            <h6 class="card-subtitle mt-3 mb-2 ps-1 text-body-secondary">Parcheggio: Sì</h6>
-            <h6 class="card-subtitle mb-2 ps-1 text-body-secondary">Distanza dal centro: tot km</h6>
-            <p class="card-text my-3 ps-2">Breve descrizione.</p>
-            <div class="text-end px-3 my-4"><p class="card-subtitle mb-2 ps-1 text-body-secondary">Voto: 4 / 5</p></div>
+            <h5 class="card-title border-bottom pb-2">
+              <?php echo $hotel['name'] ?>
+            </h5>
+
+            <h6 class="card-subtitle mt-3 mb-2 ps-1 text-body-secondary">
+              Parcheggio: <?php echo ($hotel['parking'])? 'Sì' : 'No' ?>
+            </h6>
+
+            <h6 class="card-subtitle mb-2 ps-1 text-body-secondary">
+              Distanza dal centro: <?php echo $hotel['distance_to_center'] ?> km
+            </h6>
+
+            <p class="card-text my-3 ps-2">
+              Descrizione: <?php echo $hotel['description'] ?>
+            </p>
+
+            <div class="text-end px-3 my-4">
+              <p class="card-subtitle mb-2 ps-1 text-body-secondary">
+                Voto: <?php echo $hotel['vote'] ?> / 5
+              </p>
+            </div>
+
             <div class="text-center"><a href="#" class="card-link">Vai sul sito!</a></div>
 
           </div>
@@ -76,7 +93,7 @@
         </div>
 
       </div>
-      <?php //endforeach; ?>
+      <?php endforeach; ?>
 
 
     </div>
