145799b22be77daee10f41f7c9725f3eb8b852fe
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-26 13:43:10 +0200

    [add]: static hotel card in html

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+
+  $hotels = [
+
+    [
+      'name' => 'Hotel Belvedere',
+      'description' => 'Hotel Belvedere Descrizione',
+      'parking' => true,
+      'vote' => 4,
+      'distance_to_center' => 10.4
+    ],
+    [
+      'name' => 'Hotel Futuro',
+      'description' => 'Hotel Futuro Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 2
+    ],
+    [
+      'name' => 'Hotel Rivamare',
+      'description' => 'Hotel Rivamare Descrizione',
+      'parking' => false,
+      'vote' => 1,
+      'distance_to_center' => 1
+    ],
+    [
+      'name' => 'Hotel Bellavista',
+      'description' => 'Hotel Bellavista Descrizione',
+      'parking' => false,
+      'vote' => 5,
+      'distance_to_center' => 5.5
+    ],
+    [
+      'name' => 'Hotel Milano',
+      'description' => 'Hotel Milano Descrizione',
+      'parking' => true,
+      'vote' => 2,
+      'distance_to_center' => 50
+    ],
+
+  ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous"/>
+
+  <title>PHP Hotel</title>
+</head>
+<body>
+  
+  <div class="container my-5">
+    <div class="row">
+
+      <?php //foreach ($hotels as $hotel): ?>
+      <div class="col">
+
+        <div class="card" style="width: 18rem;">
+
+          <div class="card-body">
+
+            <h5 class="card-title border-bottom pb-2"> Nome albergo <?php //echo $hotel['name'] ?> </h5>
+            <h6 class="card-subtitle mt-3 mb-2 ps-1 text-body-secondary">Parcheggio: Sì</h6>
+            <h6 class="card-subtitle mb-2 ps-1 text-body-secondary">Distanza dal centro: tot km</h6>
+            <p class="card-text my-3 ps-2">Breve descrizione.</p>
+            <div class="text-end px-3 my-4"><p class="card-subtitle mb-2 ps-1 text-body-secondary">Voto: 4 / 5</p></div>
+            <div class="text-center"><a href="#" class="card-link">Vai sul sito!</a></div>
+
+          </div>
+
+        </div>
+
+      </div>
+      <?php //endforeach; ?>
+
+
+    </div>
+  </div>
+
+</body>
+</html>
